a264f26bd1ab1979f61034f7a435c5271d706e87
fairytree <szhang.bc@gmail.com>
2026-05-12 14:09:45 -0700

    reformat

--- vicon_receiver/include/vicon_receiver/communicator.hpp
+++ vicon_receiver/include/vicon_receiver/communicator.hpp
@@ -1,16 +1,16 @@
 #if !defined(COMMUNICATOR_HPP)
 #define COMMUNICATOR_HPP
 
-#include "vicon-datastream-sdk/DataStreamClient.h"
-#include "rclcpp/rclcpp.hpp"
+#include "boost/thread.hpp"
 #include "publisher.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "vicon-datastream-sdk/DataStreamClient.h"
+#include <chrono>
 #include <iostream>
 #include <map>
-#include <chrono>
+#include <set>
 #include <string>
 #include <unistd.h>
-#include "boost/thread.hpp"
-#include <set>
 
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/transform_stamped.hpp"
@@ -19,8 +19,8 @@
 // #include "tf2/LinearMath/Transform.h"
 
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include "tf2_ros/transform_broadcaster.h"
 #include "tf2_ros/static_transform_broadcaster.h"
+#include "tf2_ros/transform_broadcaster.h"
 
 using namespace std;
 
@@ -28,43 +28,43 @@ using namespace std;
 class Communicator : public rclcpp::Node
 {
 private:
-    ViconDataStreamSDK::CPP::Client vicon_client;
+  ViconDataStreamSDK::CPP::Client vicon_client;
 
-    string hostname;
-    unsigned int buffer_size;
-    string ns_name;
-    map<string, Publisher> pub_map;
-    boost::mutex mutex;
-    std::set<std::string> pending_publishers;
-    
-    geometry_msgs::msg::TransformStamped static_tf;
-    string world_frame;
-    string vicon_frame;
-    vector<double> map_xyz;
-    vector<double> map_rpy;
-    bool map_rpy_in_degrees;
+  string hostname;
+  unsigned int buffer_size;
+  string ns_name;
+  map<string, Publisher> pub_map;
+  boost::mutex mutex;
+  std::set<std::string> pending_publishers;
 
-    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_static_broadcaster_;
-    std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+  geometry_msgs::msg::TransformStamped static_tf;
+  string world_frame;
+  string vicon_frame;
+  vector<double> map_xyz;
+  vector<double> map_rpy;
+  bool map_rpy_in_degrees;
 
-    void publish_static_transform();
+  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_static_broadcaster_;
+  std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+  void publish_static_transform();
 
 public:
-    Communicator();
+  Communicator();
 
-    // Initialises the connection to the DataStream server
-    bool connect();
+  // Initialises the connection to the DataStream server
+  bool connect();
 
-    // Stops the current connection to a DataStream server (if any).
-    bool disconnect();
+  // Stops the current connection to a DataStream server (if any).
+  bool disconnect();
 
-    // Main loop that request frames from the currently connected DataStream server and send the 
-    // received segment data to the Publisher class.
-    void get_frame();
+  // Main loop that request frames from the currently connected DataStream server and send the
+  // received segment data to the Publisher class.
+  void get_frame();
 
-    // functions to create a segment publisher in a new thread
-    void create_publisher(const string subject_name, const string segment_name);
-    void create_publisher_thread(const string subject_name, const string segment_name);
+  // functions to create a segment publisher in a new thread
+  void create_publisher(const string subject_name, const string segment_name);
+  void create_publisher_thread(const string subject_name, const string segment_name);
 };
 
 #endif // COMMUNICATOR_HPP
--- vicon_receiver/include/vicon_receiver/publisher.hpp
+++ vicon_receiver/include/vicon_receiver/publisher.hpp
@@ -1,23 +1,23 @@
 #ifndef PUBLISHER_HPP
 #define PUBLISHER_HPP
-#include <unistd.h>
-#include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include <unistd.h>
 
 // Class that allows segment data to be published in a ROS2 topic.
 class Publisher
 {
 private:
-    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr position_publisher_;
+  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr position_publisher_;
 
 public:
-    bool is_ready = false;
+  bool is_ready = false;
 
-    Publisher(std::string topic_name, rclcpp::Node* node);
+  Publisher(std::string topic_name, rclcpp::Node *node);
 
-    // Publishes the given position in the ROS2 topic whose name is indicated in
-    // the constructor.
-    void publish(geometry_msgs::msg::PoseStamped p);
+  // Publishes the given position in the ROS2 topic whose name is indicated in
+  // the constructor.
+  void publish(geometry_msgs::msg::PoseStamped p);
 };
 
 #endif
